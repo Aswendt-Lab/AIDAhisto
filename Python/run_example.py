f9f7b130e25a61d393075b3926af58a2c052ef32
pallastn <niklas.pallast@uk-koeln.de>
2019-07-09 09:24:30 +0200

    update run_example

--- Python/run_example.py
+++ Python/run_example.py
@@ -11,7 +11,7 @@ import os
 
 def main():
 
-    os.system('python AIDAhisto.py ../testImages/wholebrain_slice.tif -f -w 9 -d -c 0 -a ../testImages/wholebrain_atlas.tif -l ./acronyms_ARA.txt')
+    os.system('python AIDAhisto.py ../testImages/wholebrain_slice.tif -f -w 9 -d -c 0 -a ../testImages/wholebrain_atlas.tif -l ../ARA/acronyms_ARA.txt ')
     os.system('python AIDAhisto.py ../testImages/GFAP_lowResolution.tif -w 8 -c 2')
     os.system('python AIDAhisto.py ../testImages/GFAP_lowResolution.tif -w 33 -b -d -c 0 -r ../testImages/GFAP_lowResolution_ch3_cC.tif')
 
